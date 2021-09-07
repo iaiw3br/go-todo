243c709caf74daf9258a8281ac2d3bd1aaf30a18
Артем Иванов <iaiw3br@gmail.com>
2021-09-07 23:20:15 +0300

    feat: Добавлены маршрутизации запросов и написаны проверки на возможные ошибки

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,13 +5,63 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, _ *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.NotFound(w, r)
+		return
+	}
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Домашнаяя страница"))
+}
+
+func showAll(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Показать задачи"))
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Создать задачу"))
+}
+
+func delete(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		w.Header().Set("Allow", http.MethodDelete)
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Создать задачу"))
+	w.Write([]byte("Удалить задачу"))
+}
+
+func update(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Обновить задачу"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/todo", showAll)
+	mux.HandleFunc("/todo/create", create)
+	mux.HandleFunc("/todo/delete", delete)
+	mux.HandleFunc("/todo/update", update)
 
 	log.Println("Сервер запущен")
 
