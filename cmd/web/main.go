59858d6621cf24686a3fc0c103650ad0dfcacffd
Артем Иванов <iaiw3br@gmail.com>
2021-09-08 20:27:56 +0300

    feat: Создано подключение к бд

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/lib/pq"
 	"go-todo/pkg/models"
 	"log"
 	"net/http"
@@ -12,6 +14,16 @@ func main() {
 	localAddress := flag.String("localAddress", ":8080", "Адрес веб-сервиса")
 	errLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.Lshortfile)
+	connectionToDB := "postgres://postgres:postgres@localhost/go-todolist?sslmode=disable"
+
+	db, err := openDB(connectionToDB)
+
+	if err != nil {
+		errLog.Fatal(err)
+		return
+	}
+
+	defer db.Close()
 
 	app := application{
 		errorLog: errLog,
@@ -25,7 +37,7 @@ func main() {
 	}
 
 	infoLog.Println("Сервер запущен")
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	errLog.Fatal(err)
 }
 
@@ -33,4 +45,19 @@ type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
 	TodoList *models.TodoList
+}
+
+func openDB(connectionToDB string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", connectionToDB)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, err
 }
